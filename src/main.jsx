2f82c3114be92245b3b65e775027165ca036ba56
Yash Joshi <yashjoshi5259@gmail.com>
2026-04-26 11:35:50 +0530

    delete app.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.jsx'
 import Clock from './Clock.jsx'
 createRoot(document.getElementById('root')).render(
 
